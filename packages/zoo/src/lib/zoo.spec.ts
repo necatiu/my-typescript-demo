d8f76e3221d7c60f0b7fb72e80d110841a34c42c
necatiu <necatiu@chillipharm.com>
2025-12-28 21:12:20 +0000

    fixed lint

--- packages/zoo/src/lib/zoo.spec.ts
+++ packages/zoo/src/lib/zoo.spec.ts
@@ -3,6 +3,6 @@ import { zoo } from './zoo.js';
 describe('zoo', () => {
   it('should return animal sound', () => {
     const result = zoo();
-    expect(result).toMatch(/^(cow|pig|sheep) says (moo|oink|baa)!$/);
+    expect(result).toMatch(/^(cow|dog|pig) says (moo|woof|oink)!$/);
   });
 });
